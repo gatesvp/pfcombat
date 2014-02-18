1d5da4a2d4938731c2ca9396f8f4570143bc493f
Gaëtan Voyer-Perrault <gatesvp@gmail.com>
2014-02-18 00:27:47 -0500

    Integer and base range validation

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,3 +1,4 @@
+var INTEGER_REGEXP = /^\-?\d+$/;
 
 angular.module('pfcombat.directives', [])
   .directive('ngInitData', function() {
@@ -13,6 +14,24 @@ angular.module('pfcombat.directives', [])
           scope.init(obj);
         }
         return;
+      }
+    };
+  })
+  .directive('integer', function() {
+    return {
+      require: 'ngModel',
+      link: function(scope, elm, attrs, ctrl) {
+        ctrl.$parsers.unshift(function(viewValue) {
+          if (INTEGER_REGEXP.test(viewValue)) {
+            // it is valid
+            ctrl.$setValidity('integer', true);
+            return viewValue;
+          } else {
+            // it is invalid, return undefined (no model update)
+            ctrl.$setValidity('integer', false);
+            return undefined;
+          }
+        });
       }
     };
   });
